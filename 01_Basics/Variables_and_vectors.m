446cd500f12cdbfb81226e636abf5eaf401be00a
Víctor Díaz González <166938401+VictorDG12000GitHub@users.noreply.github.com>
2025-05-31 18:40:09 +0200

    Update Variables_and_vectors.m

--- 01_Basics/Variables_and_vectors.m
+++ 01_Basics/Variables_and_vectors.m
@@ -6,3 +6,9 @@ x*y
 %MATLAB is not strong typed, and doesn´t need ;. MATLAB also uses complex
 %numbers.
 z=sqrt(-9)
+%rand() is used to create a matrix of random numbers, the number inside the 
+() is the size of the square matrix.
+r=rand(1)
+%To create this random matrix with another size:
+ra=rand(2,5)
+%The size of ra is 2 x 5, 2 rows and 5 columns.
