f6c4e1261c9488db240f947a72ec1d357a7e623d
Víctor Díaz González <166938401+VictorDG12000GitHub@users.noreply.github.com>
2025-05-31 18:26:48 +0200

    Add files via upload

---
+++ 01_Basics/Variables_and_vectors.m
@@ -0,0 +1,8 @@
+%MATLAB has two main ways, the command window for quick operations and more
+%improvised code, and the scripts, like this one.
+x=[1 2 3]
+y=[3;2;1]
+x*y
+%MATLAB is not strong typed, and doesn´t need ;. MATLAB also uses complex
+%numbers.
+z=sqrt(-9)
